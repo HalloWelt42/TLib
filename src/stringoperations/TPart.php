43b318ada4554f63594da0435a059fc185e3da66
HalloWelt42 <github@texeln.de>
2020-06-27 23:56:44 +0200

    update unittest update TPart

--- src/stringoperations/TPart.php
+++ src/stringoperations/TPart.php
@@ -19,28 +19,46 @@ trait TPart
 
 
   /**
-   * @param string $str
+   * Schneidet übereinstimmenden Prfixe vom String ab und gibt das ergebnis zurück
+   *
+   * @param string $str_external
    * @return $this|StringType
    */
-  public function cutPreFix( $str ){
+  public function cutPreFix( $str_external ){
 
-    $str_len_external = strlen($str );
+    $str_len_external = strlen($str_external );
     $str_len_internal = strlen( $this->str );
-    $str_len_div = $str_len_internal - $str_len_external;
 
-    if( $str_len_div <= 0 ){
+    /**
+     * auf gleiche Länge bringen
+     */
+    if( $str_len_internal - $str_len_external <= 0 ){
       $str_len_external = $str_len_internal;
+      $str_external = substr( $str_external , 0 , $str_len_external);
     }
 
-    $sub_str = substr( $this->str , 0 , $str_len_external);
+    /**
+     * Inhalt vergleichen, bei gleich langen Strings
+     */
+    if( $str_external === $this->str ){
+      return new StringType('');
+    }
 
-    if ($sub_str === $str) {
+    /**
+     * Differenz ausgeben, wenn Inhalt im Prefix gleich ist
+     */
+    $prefix = substr( $this->str , 0,$str_len_external);
+    if( $prefix === $str_external ){
       return new StringType(
-          substr($this->str, $str_len_external, $str_len_div)
+          substr( $this->str , $str_len_external , $str_len_internal )
       );
     }
 
-    return new StringType($this -> str );
+    /**
+     * Keine übereinstimmungen -> ohne Veränderung ausgeben
+     */
+    return $this;
+
   }
 
 
--- test/unittests/TestStringFunctionPart.php
+++ test/unittests/TestStringFunctionPart.php
@@ -13,33 +13,45 @@ class TestStringFunctionPart extends TestCase
   public function testPart(){
 
     $this->assertEquals(
-        'World!',
-        (string)( new StringType('HalloWorld!'))->cutPreFix('Hallo')
+          'World!',
+          (string)( new StringType('HalloWorld!'))->cutPreFix('Hallo'),
+        'HalloWorld! - Hallo = World! '
+      );
+
+    $this->assertEquals(
+        'HalloWorld!',
+        (string)( new StringType('HalloWorld!'))->cutPreFix('HALLO'),
+        'HalloWorld! - HALLO = HalloWorld!'
     );
 
     $this->assertEquals(
         'HalloWorld!',
-        (string)( new StringType('HalloWorld!'))->cutPreFix('HALLO')
+        (string)( new StringType('HalloWorld!'))->cutPreFix('HALLOWORLD!!!'),
+        'HalloWorld! - HALLOWORLD!!! = HalloWorld!'
     );
 
     $this->assertEquals(
         'HalloWorld!',
-        (string)( new StringType('HalloWorld!'))->cutPreFix('oW')
+        (string)( new StringType('HalloWorld!'))->cutPreFix('oW'),
+        'HalloWorld! - oW = HalloWorld!'
     );
 
     $this->assertEquals(
         '!',
-        (string)( new StringType('HalloWorld!'))->cutPreFix('HalloWorld')
+        (string)( new StringType('HalloWorld!'))->cutPreFix('HalloWorld'),
+        'HalloWorld! - HalloWorld = !'
     );
 
     $this->assertEquals(
         '',
-        (string)( new StringType('HalloWorld!'))->cutPreFix('HalloWorld!')
+        (string)( new StringType('HalloWorld!'))->cutPreFix('HalloWorld!'),
+        'HalloWorld! - HalloWorld! = '
     );
 
     $this->assertEquals(
         '',
-        (string)( new StringType(''))->cutPreFix('HalloWorld!123')
+        (string)( new StringType('HalloWorld!'))->cutPreFix('HalloWorld!123'),
+        'HalloWorld! - HalloWorld!123 = '
     );
 
 
